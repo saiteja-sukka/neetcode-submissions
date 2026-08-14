fadadf7f297f4a59516421d8915dc6de4d7b37bf
saiteja-sukka <160473795+saiteja-sukka@users.noreply.github.com>
2026-08-15 00:26:46 +0530

    Add: python-comments - submission-0

---
+++ Python For Beginners/python-comments/submission-0.py
@@ -0,0 +1,32 @@
+print("I belong here.")
+# print("Why am I here?")
+# print("Get me out of here!")
+print("I also belong here.")
+print("Me too!")
+
+
+# Comments
+# In programming we can annotate our code without affecting the output. This is done using comments.
+
+# Comments are ignored by the interpreter. They are used to explain what the code is doing or add any sort of message, usually for humans. They are useful for other programmers who may read your code, or for yourself in the future if you forget what a certain piece of code does.
+
+# In Python, comments are created using the # character. Any text after the # character on a line is considered a comment.
+
+# # This is a comment
+# If we want to ignore a piece of code, we can also comment it out temporarily. This is useful for debugging or testing.
+
+# # print("This line of code is commented out")
+# The above code will not print anything to the console because the line is commented out.
+
+# Challenge
+# Your task is to fix the code on the right without deleting any of the lines. It should print the following text to the console:
+
+# I belong here.
+# I also belong here.
+# Me too!
+# If you think you have the correct answer, click the Submit button.
+
+
+# Click me for a useful tip.
+
+# A short cut to comment out lines of code in most code editors is to select the lines you want to comment out and press Ctrl + / (Windows / Linux) or Cmd + / (Mac). This will automatically add the # character to the beginning of each line.
