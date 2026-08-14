3ef5cd0c0a39362d3482a218898c91273d22fbfa
saiteja-sukka <160473795+saiteja-sukka@users.noreply.github.com>
2026-08-15 00:37:54 +0530

    Add: python-naming-conventions - submission-0

---
+++ Python For Beginners/python-naming-conventions/submission-0.py
@@ -0,0 +1,32 @@
+python_creation_date = "February, 1991"
+java_creation_date = "May, 1995"
+javascript_creation_date = "December, 1995"
+
+print(python_creation_date)
+print(java_creation_date)
+print(javascript_creation_date)
+
+
+
+
+
+
+
+
+
+
+
+""" Naming Conventions
+More than having good variable names, it's important to follow naming conventions. These are rules that help us write code that is easy to read and understand.
+
+There are several different naming conventions such as:
+
+Camel Case: The first letter of each word is capitalized except for the first word. For example, myVariableName.
+Snake Case: Words are separated by underscores. For example, my_variable_name.
+Pascal Case: The first letter of each word is capitalized. For example, MyVariableName.
+In Python, we typically use snake case. This means we use underscores (_) to separate words in variable names.
+
+Challenge
+Rename the variables in the code editor to follow the snake case naming convention.
+
+If you think you have the correct answer, click the Submit button. """
