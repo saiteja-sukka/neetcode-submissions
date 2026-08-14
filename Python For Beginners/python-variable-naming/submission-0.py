fab7933b195b7d8a8fb084f0ceaf9256d84bb2c5
saiteja-sukka <160473795+saiteja-sukka@users.noreply.github.com>
2026-08-15 00:35:15 +0530

    Add: python-variable-naming - submission-0

---
+++ Python For Beginners/python-variable-naming/submission-0.py
@@ -0,0 +1,38 @@
+capital_of_spain = "Madrid"
+capital_of_france = "Paris"
+capital_of_germany="Berlin"
+
+print(capital_of_spain)
+print(capital_of_france)
+print(capital_of_germany)
+
+""" Variable Naming
+Previously we named our string variable message. The name of a variable is how we can refer to it in our code. This means we can't have two different variables with the same name.
+
+If we wanted a variable to store the capital of the United Kingdom, we could name it capital_of_uk. This is a good name because it is descriptive and easy to understand.
+
+capital_of_uk = "London"
+print(capital_of_uk)
+Output:
+
+London
+There are some strict rules for naming variables, if you break any of these rules it will cause an error:
+
+Variable names can only contain letters, numbers, and underscores.
+Variable names can't start with a number.
+Variable names can't contain spaces.
+Variable names can't be the same as Python keywords like for, if, (we will learn more about these later).
+Challenge
+You are given some incomplete code on the right. It should print the capitals of Spain, France and Germany, in that order. Try to use some descriptive variable names to complete the code. The correct output should be:
+
+Madrid
+Paris
+Berlin
+
+Helpful tips for naming.
+
+The names we give our variables are important. They should be descriptive and easy to understand. This makes our code easier to read and maintain. In large code bases, you may easily forget what a variable does if it has a poor name.
+
+This sounds easy to do, but in practice giving variables good names can be difficult. It's a skill that comes with practice.
+
+"""
