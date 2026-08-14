c34ad90398ee5569a50ce4ead9b470826862df73
saiteja-sukka <160473795+saiteja-sukka@users.noreply.github.com>
2026-08-15 00:30:50 +0530

    Add: python-variable-declaration - submission-0

---
+++ Python For Beginners/python-variable-declaration/submission-0.py
@@ -0,0 +1,33 @@
+message="this string is stored in a variable"
+# don't modify the code below this line
+print(message)
+print(message)
+
+
+# Variable Declaration
+# Earlier we printed the "Hello, world!" string to the console. After it was printed we couldn't reuse that string, unless we retyped it all out from scratch, as shown below:
+
+# print("Hello, world!")
+# print("Hello, world!")
+# But what if we wanted to use that string multiple times in our code? This is where variables come in. With the following code, we can store the string in a variable and print it multiple times:
+
+# message = "Hello, world!"
+# print(message)
+# print(message)
+# Output:
+
+# Hello, world!
+# Hello, world!
+# In the code above, we stored the string "Hello, world!" in a variable called message. Then, instead of passing a raw string to the print() function, we passed the variable message. This way, we can print the same string multiple times without having to retype it.
+
+# Variables are like containers that hold values.
+
+# Challenge
+# Update the code on the right so that it prints this string is stored in a variable to the console twice. You should be able to accomplish this by only adding one line of code.
+
+# If you think you have the correct answer, click the Submit button.
+
+
+# Variable vs Raw String
+
+# Notice that we don't have double quotes around message. If we did, Python would think we're trying to print the string "message" instead of the value stored in the variable message.
