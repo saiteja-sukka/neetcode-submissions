757069627a228801493865782e16f1d5b48f64ac
saiteja-sukka <160473795+saiteja-sukka@users.noreply.github.com>
2026-08-15 21:20:10 +0530

    Add: python-for-loops - submission-0

---
+++ Python For Beginners/python-for-loops/submission-0.py
@@ -0,0 +1,68 @@
+for i in range(13):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" For Loops
+There is a second way of looping in Python, using the for keyword. This is called a for loop.
+
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+
+for i in range(10):
+    print(i)
+The above two loops are equivalent. The first loop uses a while loop and the second loop uses a for loop. Both loops will print the numbers 0 through 9. You can see that the for loop is more concise and easier to read.
+
+Let's explain the syntax of a for loop:
+
+The loop starts with the keyword for.
+Next, you need to specify a variable name. This variable will represent each item in the sequence. In the example above, we used the variable name i.
+After the variable name, you need to use the keyword in.
+Finally, you need to specify the sequence you want to iterate over. In the example above, we used the function range(10). This function generates a sequence of numbers from 0 to 9. 10 is not included in the sequence.
+Challenge
+Using a for loop, print the numbers 0 through 12, in order, including 0 and 12. """
