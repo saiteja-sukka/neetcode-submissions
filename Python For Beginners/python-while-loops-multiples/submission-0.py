f00d2eafc199a4247a2f9d92ec4a6f3d93353e2e
saiteja-sukka <160473795+saiteja-sukka@users.noreply.github.com>
2026-08-15 21:17:27 +0530

    Add: python-while-loops-multiples - submission-0

---
+++ Python For Beginners/python-while-loops-multiples/submission-0.py
@@ -0,0 +1,56 @@
+i=1
+while(i<10):
+    print(i*10)
+    i+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" While Loops Multiples
+In the code editor, can you use a while loop to print all multiples of 10 from 10 to 90 in order (including 10 and 90)?
+
+The basic structure of a while loop in Python is shown below for your reference.
+
+i = 0
+while i < 5:
+    print("I love Python!")
+    i += 1 """
