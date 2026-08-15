b004333f43de85d9dc80e4749256b1a5e859be0c
saiteja-sukka <160473795+saiteja-sukka@users.noreply.github.com>
2026-08-15 21:15:39 +0530

    Add: python-while-loops-counting - submission-0

---
+++ Python For Beginners/python-while-loops-counting/submission-0.py
@@ -0,0 +1,50 @@
+i=0
+while i<10:
+    print(i)
+    i+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" While Loops Counting
+In the code editor, can you use a while loop to print the digits from 0 to 9 in order (including 0 and 9)? Your loop should execute 10 times.
+
+The basic structure of a while loop in Python is shown below for your reference.
+
+i = 0
+while i < 5:
+    print("I love Python!")
+    i += 1 """
