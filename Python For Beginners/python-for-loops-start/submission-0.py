f46e9398cf05ff4fee702a26bd85ec35e9536b7d
saiteja-sukka <160473795+saiteja-sukka@users.noreply.github.com>
2026-08-15 21:21:46 +0530

    Add: python-for-loops-start - submission-0

---
+++ Python For Beginners/python-for-loops-start/submission-0.py
@@ -0,0 +1,57 @@
+for i in range(10,21):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" For Loops Start
+It would be very limiting if for loops could only start at 0. Well the good news is that they don't have to! You can start a for loop at any number you want. Simply pass two arguments in to the range() function.
+
+i = 2
+while i < 5:
+    print(i)
+    i += 1
+
+for i in range(2, 5):
+    print(i)
+The above two loops are equivalent. The first parameter to the range() function is the starting number, and the second parameter is the ending number. The ending number is not included in the sequence. Each loops will print the numbers 2 through 4.
+
+Challenge
+Using a for loop, print the numbers 10 through 20, in order, including 10 and 20. """
