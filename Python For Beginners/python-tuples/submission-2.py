2255740771ade9a32cf12f9f28c9be8bac83c8e1
saiteja-sukka <160473795+saiteja-sukka@users.noreply.github.com>
2026-08-17 12:21:17 +0530

    Add: python-tuples - submission-2

---
+++ Python For Beginners/python-tuples/submission-2.py
@@ -0,0 +1,78 @@
+from typing import Tuple # this is to add type hints for tuples
+
+def create_pair(name: str, age: int) -> Tuple[str, int]:
+    tuples=(name,age)
+    return tuples
+    pass
+
+# do not modify code below this line
+print(create_pair("Alice", 25))
+print(create_pair("Bob", 30))
+print(create_pair("Charlie", 35))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" Tuples
+Tuples are very similar to lists, but they have one key difference: they are immutable. This means that once a tuple is created, it cannot be changed. We can create a tuple by using parentheses instead of square brackets:
+
+my_tuple = (4, 5, 6)
+
+print(my_tuple)  # Output: (4, 5, 6)
+We can index it just like a list:
+
+my_tuple = (4, 5, 6)
+
+print(my_tuple[0])  # Output: 4
+print(my_tuple[1])  # Output: 5
+print(my_tuple[2])  # Output: 6
+We can also use slicing:
+
+my_tuple = (4, 5, 6)
+
+print(my_tuple[1:])  # Output: (5, 6)
+Keep in mind, slicing a tuple doesn't modify it, instead it creates a new tuple with the specified slice.
+
+Since we can't modify a tuple, the following code will raise an error:
+
+my_tuple = (4, 5, 6)
+
+my_tuple[0] = 1 # Raises an error
+We also can't call append or pop on a tuple, since these functions would modify it. We can however still call sum(), max(), and min() on a tuple, since these functions don't modify the tuple.
+
+It's common to use tuples to store related data.
+
+Challenge
+Implement a function create_pair(name: str, age: int) -> Tuple, which should combine the name and age parameters into a tuple and return it. The tuple should contain the name as the first element and the age as the second element. """
