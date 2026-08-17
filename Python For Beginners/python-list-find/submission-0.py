9ca86a8961ba60fb39b7d496960506d9fde0fc04
saiteja-sukka <160473795+saiteja-sukka@users.noreply.github.com>
2026-08-17 11:52:33 +0530

    Add: python-list-find - submission-0

---
+++ Python For Beginners/python-list-find/submission-0.py
@@ -0,0 +1,69 @@
+from typing import List # this is used to add type hints for List type
+
+def find_index(nums: List[int], target: int) -> int:
+    return nums.index(target)
+    pass
+
+
+# don't modify code below this line
+print(find_index([1, 2, 3, 4, 5], 3))
+print(find_index([1, 2, 3, 4, 5, 3], 3))
+print(find_index([1, 2, 3, 4], 1))
+print(find_index([1, 3, 4, 2], 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" List Find
+We learned that we can determine if a list contains a specific element with the in operator. We can also find the index of an element in a list using the index() function.
+
+my_list = [1, 2, 3, 4, 5, 3]
+
+print(my_list.index(3))  # Output: 2
+The above code snippet will print the index of the first occurence of the element 3 in the list my_list. There are two 3s in the list, but the first one is at index 2, so 2 is printed. If the element is not present in the list, a ValueError will be raised.
+
+Challenge
+Implement the find_index(nums: List[int], target: int) -> int function. It should return the index of the first occurrence of the target number in the list. You may assume that the target number will always be present in the list.
+
+If you want a challenge, try to implement the function without using the built-in index() method. """
