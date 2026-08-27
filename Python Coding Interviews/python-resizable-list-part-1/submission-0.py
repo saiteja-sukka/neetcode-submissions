2a0294d34854d54bc7d00908dd14b34065b27016
saiteja-sukka <160473795+saiteja-sukka@users.noreply.github.com>
2026-08-27 16:41:20 +0530

    Add: python-resizable-list-part-1 - submission-0

---
+++ Python Coding Interviews/python-resizable-list-part-1/submission-0.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+def append_elements(arr1: List[int], arr2: List[int]) -> List[int]:
+    return arr1+arr2
+    pass
+
+
+def pop_n(arr: List[int], n: int) -> List[int]:
+    if n>len(arr):
+        return []
+    else:
+        while(n>0):
+            arr.pop()
+            n-=1
+    return arr
+    pass
+
+
+def insert_at(arr: List[int], index: int, element: int) -> List[int]:
+    arr.insert(index,element)
+    return arr
+    pass
+
+
+# do not modify below this line
+print(append_elements([1, 2, 3], [4, 5, 6]))
+print(append_elements([4, 3], [4, 5, 3]))
+
+print(pop_n([1, 2, 3, 4, 5], 2))
+print(pop_n([1, 2, 3, 4, 5], 6))
+print(pop_n([1, 2, 3, 4, 5], 5))
+
+print(insert_at([1, 2, 3, 4, 5], 2, 6))
+print(insert_at([1, 2, 3, 4], 6, 5))
