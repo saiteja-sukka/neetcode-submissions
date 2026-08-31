ea29e86a427fe00d677d4e7268e3c36b28781e4b
saiteja-sukka <160473795+saiteja-sukka@users.noreply.github.com>
2026-08-31 16:51:42 +0530

    Add: sql-common-table-expressions - submission-2

---
+++ SQL/sql-common-table-expressions/submission-2.sql
@@ -0,0 +1,49 @@
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT
+);
+
+CREATE TABLE orders (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  customer_id INTEGER,
+  price INTEGER
+);
+
+INSERT INTO customers (name) VALUES
+  ('Alice'),
+  ('Bob'),
+  ('Charlie'),
+  ('David'),
+  ('Eve'),
+  ('Frank'),
+  ('Grace'),
+  ('Hank');
+
+INSERT INTO orders (customer_id, price) VALUES
+  (1, 50),
+  (2, 100),
+  (3, 150),
+  (4, 200),
+  (5, 250),
+  (6, 30),
+  (7, 70),
+  (8, 400);
+-- Do not modify above this line. --
+
+
+
+
+
+
+
+SELECT c.name 
+FROM customers c
+JOIN orders o ON c.id=o.customer_id
+GROUP BY c.name
+HAVING MAX(o.price) <100
+ORDER BY c.name 
+
+
+
+
+
